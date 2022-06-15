27e650e49bd7af5a0a6bacc95ed6513ac65eb309
rizkysr90 <rizkysr90@gmail.com>
2022-06-15 20:45:19 +0700

    create:modeling database

---
+++ src/migrations/20220615133825-create-product-image.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Product_images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      url_image: {
+        type: Sequelize.STRING
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Product_images');
+  }
+};
---
+++ src/models/product.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.STRING,
+    description: DataTypes.STRING,
+    isActive: DataTypes.BOOLEAN,
+    status: DataTypes.BOOLEAN,
+    id_user: DataTypes.INTEGER,
+    id_category: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
