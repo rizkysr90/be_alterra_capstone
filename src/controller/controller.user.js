b251d0f2ea1b485baf97650729d573624d866999
rizkysr90 <rizkysr90@gmail.com>
2022-06-16 15:35:25 +0700

    add:api/users/register

--- src/controller/controller.user.js
+++ src/controller/controller.user.js
@@ -1,10 +1,62 @@
+const {User} = require('./../models/');
+const response = require('./../utility/responseModel');
+const bcrypt = require('../utility/bcrypt');
 const dataUserAll = (req,res) => {
     res.status(200).json({
         messege : 'Succcess'
     })
 }
+const createUser = async (req,res) => {
+    // Pakai try catch untuk handle error by server agar bisa ditangkap
+    try {
+        // Di req.body akan ada data = {email,password,name} untuk register
+        const {email,password,name} = req.body
+        // Email harus unique,jadi sebelum create melakukan validasi
+        // dengan cara findOne dengan menggunakan email
+        const findUserByEmail = await User.findOne({
+            where : {
+                email
+            }
+        })
+        // Jika ditemukan/true maka kembalikan response error dan jika false maka bisa membuat user
+        if (findUserByEmail) {
+            // response.error merupakan utility yang dibuat di folder utility/responseModel.js
+            res.status(400).json(response.error(400,'Email sudah digunakan'))
+        }
+        // Sebelum create user di database,password harus di enkripsi terlebih dahulu
+        const hashedPassword = await bcrypt.hash(password);
+        // Buat data yang akan dimasukkan ke database,karna untuk data profile secara default harus null
+        // jadi untuk menambahkan data profile bisa melakukan update user saja
+        const dataToBeInsertToDatabase = {
+            email : email,
+            password : hashedPassword,
+            profile_picture : null,
+            phone_number : null,
+            address : null,
+            city_id : null,
+            name : name
+        }
+        // Membuat user ke database
+        const createUser = User.create(dataToBeInsertToDatabase);
+        // Jika berhasil,buat data untuk diberikan pada response
+        // data yang credential seperti password,email,address tidak usah dikirim di response
+        // kecuali jika dibutuhkan
+        const dataToBeSentToResponse = {
+            id : createUser.id,
+            name : createUser.name,
+        }
+        res.status(201).json(response.success(200,dataToBeSentToResponse));
+    } catch(err) {
+        res.status(500).json(response.error(500,'Internal Server Error'));
+    }
+    
+
+
+
+}
 
 
 module.exports = {
-    dataUserAll
+    dataUserAll,
+    createUser
 }
---
+++ src/middleware/expressValidator.js
@@ -0,0 +1,22 @@
+const { validationResult } = require('express-validator');
+const response = require('./../utility/responseModel');
+
+const validate = (req,res,next) => {
+    try {
+        // Mengambil semua error yang terjadi saat melakukan validasi di middleware sebelumnya
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // Semua data error disimpan di variabel errors.array()
+            const dataErrorFromExpressValidator = errors.array();
+            res.status(400).json(response.error(400,dataErrorFromExpressValidator));
+        } else {
+            // Jika tidak ada error,maka akan masuk ke controller
+            next();
+        }
+
+    } catch (error) {
+        res.status(500).json(response.error(500,'Internal Server Error'))
+    }
+}
+
+module.exports = validate;
