5d49e710b2a575a2b74091ac625c2b7d486ee807
rizkysr90 <rizkysr90@gmail.com>
2022-06-17 03:01:24 +0000

    Api/users  login

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const response = require('../utility/responseModel');
 const routerUser = require('./user')
+const routerRegister = require('./register');
 // const routerProduct = require('./product')
 // const routerCategories = require('./categories')
 // const routerProductImages = require('./product_images')
 // const routerTransactions = require('')
-
+router.use(`${process.env.URL_ROUTER_REGISTER}`,routerRegister)
 router.use(`${process.env.URL_ROUTER_USER}`, routerUser)
 // router.use(`${process.env.URL_ROUTER_PRODUCT}`, routerProduct)
 // router.use(`${process.env.URL_ROUTER_CATEGORIES}`, routerCategories)
