3f645ab81d1a960f04ef999b6ee174186e7c343c
rizkysr90 <rizkysr90@gmail.com>
2022-07-01 18:42:33 +0700

    fix route in myproducts

--- src/router/myproducts.js
+++ src/router/myproducts.js
@@ -8,7 +8,6 @@ const sanitationMyProducs = require('../middleware/validator/myProduct.sanitatio
 const authJWT = require('./../middleware/passport-jwt');
 
 router.get('/', authJWT, controllerProduct.dataProductAll)
-router.get('/terjual', authJWT, controllerProduct.dataProductAllTerjual)
 router.get('/:id', authJWT, controllerProduct.dataProductById)
 router.post('/', authJWT,upload.array('gambar'), MulterError,sanitationMyProducs.sanitationDataMyProduct, myproductsValidator.create(), validate, controllerProduct.createDataProduct)
 router.put('/:id', authJWT,upload.array('gambar'), MulterError,sanitationMyProducs.sanitationDataMyProduct, myproductsValidator.create(), validate, controllerProduct.updateDataProduct)
