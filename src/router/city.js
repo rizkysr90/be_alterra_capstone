bec38149242ae67fded371452e3622db5ccbe347
rizkysr90 <rizkysr90@gmail.com>
2022-07-01 15:05:10 +0700

    fix:pagination

--- src/router/city.js
+++ src/router/city.js
@@ -3,7 +3,6 @@ const cityController = require('./../controller/controller.city');
 const authJWT = require('./../middleware/passport-jwt');
 
 router.get('/',authJWT,cityController.getAllDataCities);
-router.get('/search',authJWT,cityController.searchByName);
 
 
 
