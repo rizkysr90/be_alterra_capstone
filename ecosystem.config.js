716dd4ba82903d462c797c5b5291fb4addd46b86
rizkysr90 <rizkysr90@gmail.com>
2022-07-23 16:53:25 +0700

    edit pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name: "secondhand_app",
-      script: "bin/www",
+      script: "bin/www.js",
       instances: "max",
       env: {
         NODE_ENV: "development",
