0be9dd4e65eef1347c1f6c79dd199dbc84588c4b
rizkysr90 <rizkysr90@gmail.com>
2022-07-26 01:34:22 +0700

    revert from previous multer config

--- src/router/profile.js
+++ src/router/profile.js
@@ -5,11 +5,11 @@ const authJWT = require('./../middleware/passport-jwt');
 const validate = require('./../middleware/expressValidator');
 const userValidator = require('./../middleware/validator/user.validator');
 const userSanitize = require('./../middleware/sanitize/user.sanitize');
-const {upload,MulterError,uploadSingleMiddleware} = require('./../middleware/multer');
+const {upload,MulterError} = require('./../middleware/multer');
 
 router.get('/:user_id',authJWT,userController.getProfileById);
-router.put('/:user_id',authJWT,uploadSingleMiddleware
-            ,userSanitize.update(),userValidator.update(),
+router.put('/:user_id',authJWT,upload.single('profile_picture'),
+            MulterError,userSanitize.update(),userValidator.update(),
             validate,userController.updateProfile);
 
 module.exports = router;
